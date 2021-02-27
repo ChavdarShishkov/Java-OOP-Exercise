4499fbdde56cd02186cd7fd866ee6412a6508d52
chavdar <finaltitan69@gmail.com>
2021-02-27 18:26:06 +0200

    Commit

--- src/Exercise_01_WorkingWithAbstractions/P01_CardSuit/Main.java
+++ src/Exercise_01_WorkingWithAbstractions/P01_CardSuit/Main.java
@@ -1,7 +1,12 @@
 package Exercise_01_WorkingWithAbstractions.P01_CardSuit;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
+        CardSuit[] values = CardSuit.values();
+        System.out.println("Card Suits:");
+        Arrays.stream(values).forEach(a -> System.out.printf("Ordinal value: %d; Name value: %s%n", a.ordinal(),
+                a.name()));
     }
 }
