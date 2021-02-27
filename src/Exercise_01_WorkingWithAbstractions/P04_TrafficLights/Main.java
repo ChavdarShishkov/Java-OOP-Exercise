a321a3602bbae966db080ce8e4c69ea2976cd982
chavdar <finaltitan69@gmail.com>
2021-02-27 19:38:54 +0200

    Commit

--- src/Exercise_01_WorkingWithAbstractions/P04_TrafficLights/Main.java
+++ src/Exercise_01_WorkingWithAbstractions/P04_TrafficLights/Main.java
@@ -1,7 +1,28 @@
 package Exercise_01_WorkingWithAbstractions.P04_TrafficLights;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] initialTrafficLightsState = reader.readLine().split("\\s+");
+
+        int numberOFUpdates = Integer.parseInt(reader.readLine());
+
+        List<TrafficLight> trafficLights =
+                Arrays.stream(initialTrafficLightsState).map(s -> new TrafficLight(TrafficLightState.valueOf(s))).collect(Collectors.toList());
+
+        for (int i = 0; i < numberOFUpdates; i++) {
+            for (TrafficLight trafficLight : trafficLights) {
+                trafficLight.update();
+                System.out.print(trafficLight + " ");
+            }
+            System.out.println();
+        }
     }
 }
---
+++ src/Exercise_01_WorkingWithAbstractions/P04_TrafficLights/TrafficLightState.java
@@ -0,0 +1,5 @@
+package Exercise_01_WorkingWithAbstractions.P04_TrafficLights;
+
+public enum TrafficLightState {
+    RED, YELLOW, GREEN;
+}
