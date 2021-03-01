8bdecad7d7fe71b2ca22b1e5aef316afc47ffb32
chavdar <finaltitan69@gmail.com>
2021-03-01 17:11:51 +0200

    Commit

--- src/Exercise_01_WorkingWithAbstractions/P05_JediGalaxy/Main.java
+++ src/Exercise_01_WorkingWithAbstractions/P05_JediGalaxy/Main.java
@@ -8,28 +8,32 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int[] dimensions = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int x = dimensions[0];
-        int y = dimensions[1];
+        int row = dimensions[0];
+        int col = dimensions[1];
 
-        int[][] matrix = new int[x][y];
+        int[][] matrix = new int[row][col];
 
         int value = 0;
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < y; j++) {
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
                 matrix[i][j] = value++;
             }
         }
 
+        int sum = 0;
+
         String command = scanner.nextLine();
-        long sum = 0;
         while (!command.equals("Let the Force be with you")) {
+
             int[] ivoS = Arrays.stream(command.split(" ")).mapToInt(Integer::parseInt).toArray();
             int[] evil = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
             int xE = evil[0];
             int yE = evil[1];
 
             while (xE >= 0 && yE >= 0) {
-                if (xE >= 0 && xE < matrix.length && yE >= 0 && yE < matrix[0].length) {
+                if (xE < matrix.length && yE < matrix[0].length) {
                     matrix[xE][yE] = 0;
                 }
                 xE--;
@@ -40,19 +44,15 @@ public class Main {
             int yI = ivoS[1];
 
             while (xI >= 0 && yI < matrix[1].length) {
-                if (xI >= 0 && xI < matrix.length && yI >= 0 && yI < matrix[0].length) {
+                if (xI < matrix.length && yI >= 0 && yI < matrix[0].length) {
                     sum += matrix[xI][yI];
                 }
 
                 yI++;
                 xI--;
             }
-
             command = scanner.nextLine();
         }
-
         System.out.println(sum);
-
-
     }
 }
---
+++ src/Exercise_01_WorkingWithAbstractions/P06_GreedyTimes/Bag.java
@@ -0,0 +1,99 @@
+package Exercise_01_WorkingWithAbstractions.P06_GreedyTimes;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Bag {
+    private final long capacity;
+    private final Map<String, Long> gems;
+    private final Map<String, Long> cash;
+    private long totalItemQuantity;
+    private long gold;
+
+    public Bag(long capacity) {
+        this.capacity = capacity;
+        this.gems = new HashMap<>();
+        this.cash = new HashMap<>();
+    }
+
+    public void addCash(String item, long itemQuantity) {
+        long totalGems = totalGems();
+        long totalCash = totalCash();
+
+        if (hasFreeCapacity(itemQuantity) && totalGems >= totalCash + itemQuantity) {
+
+            if (!this.cash.containsKey(item)) {
+                this.cash.put(item, itemQuantity);
+            } else {
+                this.cash.put(item, this.cash.get(item) + itemQuantity);
+            }
+            this.totalItemQuantity += itemQuantity;
+        }
+    }
+
+    public void addGems(String item, long itemQuantity) {
+        long totalGems = totalGems();
+
+        if (hasFreeCapacity(itemQuantity) && totalGems + itemQuantity <= this.gold) {
+
+            if (!this.gems.containsKey(item)) {
+                this.gems.put(item, itemQuantity);
+            } else {
+                this.gems.put(item, this.gems.get(item) + itemQuantity);
+            }
+            this.totalItemQuantity += itemQuantity;
+        }
+    }
+
+    public void addGold(long itemQuantity) {
+
+        if (hasFreeCapacity(itemQuantity)) {
+            this.gold += itemQuantity;
+            this.totalItemQuantity += itemQuantity;
+        }
+    }
+
+    private boolean hasFreeCapacity(long itemQuantity) {
+        return itemQuantity + totalItemQuantity <= capacity;
+    }
+
+    private long totalCash() {
+        return this.cash.values().stream().mapToLong(e -> e).sum();
+    }
+
+    private long totalGems() {
+        return this.gems.values().stream().mapToLong(e -> e).sum();
+    }
+
+    private Comparator<Map.Entry<String, Long>> getComparator() {
+        return (e1, e2) -> {
+            int res = e2.getKey().compareTo(e1.getKey());
+
+            if (res == 0) {
+                res = e1.getValue().compareTo(e2.getValue());
+            }
+            return res;
+        };
+    }
+
+    public void printContent() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("<Gold> $%d", this.gold)).append(System.lineSeparator());
+        sb.append(String.format("##Gold - %d", this.gold)).append(System.lineSeparator());
+
+        if (this.gems.isEmpty()) {
+            sb.append(String.format("<Gems> $%d", totalGems())).append(System.lineSeparator());
+
+            this.gems.entrySet().stream().sorted(getComparator()).forEach(entry -> sb.append(String.format("##%s - " + "%d", entry.getKey(), entry.getValue())).append(System.lineSeparator()));
+        }
+
+        if (this.cash.isEmpty()) {
+            sb.append(String.format("<Gems> $%d", totalCash())).append(System.lineSeparator());
+
+            this.cash.entrySet().stream().sorted(getComparator()).forEach(entry -> sb.append(String.format("##%s - " + "%d", entry.getKey(), entry.getValue())).append(System.lineSeparator()));
+        }
+
+        System.out.print(sb.toString());
+    }
+}
