f3096260bfe421b0e5c58cd290c751f2cad0cf91
chavd <finaltitan69@gmail.com>
2021-03-04 20:19:47 +0200

    Commit

--- src/Exercise_02_Encapsulation/P04_PizzaCalories/Main.java
+++ src/Exercise_02_Encapsulation/P04_PizzaCalories/Main.java
@@ -1,7 +1,27 @@
 package Exercise_02_Encapsulation.P04_PizzaCalories;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] pizzaTokens = reader.readLine().split("\\s+");
+        String[] doughTokens = reader.readLine().split("\\s+");
+        int countOfToppings = Integer.parseInt(pizzaTokens[2]);
+
+        Pizza pizza = new Pizza(pizzaTokens[1], countOfToppings);
+        Dough dough = new Dough(doughTokens[1], doughTokens[2], Double.parseDouble(doughTokens[3]));
+        String input = reader.readLine();
+
+        for (int i = 0; i < countOfToppings; i++) {
+            String[] toppingTokens = reader.readLine().split("\\s+");
+            Topping topping = new Topping(toppingTokens[1], Double.parseDouble(toppingTokens[2]));
+
+            pizza.addTopping(topping);
+        }
+
+        System.out.printf("%s - %.2f", pizza.getName(), pizza.getOverallCalories());
     }
 }
