79976368a4e748e8223b59f71f9ff642962936bb
chavd <finaltitan69@gmail.com>
2021-03-05 13:30:21 +0200

    Commit

--- src/Exercise_03_Inheritance/P04_NeedForSpeed/Motorcycle.java
+++ src/Exercise_03_Inheritance/P04_NeedForSpeed/Motorcycle.java
@@ -1,6 +1,7 @@
 package Exercise_03_Inheritance.P04_NeedForSpeed;
 
 public class Motorcycle extends Vehicle {
+
     public Motorcycle(double fuel, int horsepower) {
         super(fuel, horsepower);
     }
